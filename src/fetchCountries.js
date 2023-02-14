8646f4c5aae520342b4e43d0ca20407debe1aec2
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-02-14 22:46:10 +0200

    update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,5 @@
+export default fetchCountries;
+
 function manageErrors(response) {
   if(!response.ok) {
     const responseError = { 
@@ -10,10 +12,6 @@ function manageErrors(response) {
 }
 
 function fetchCountries(inputValue) {
-  
   const URL = `https://restcountries.com/v3.1/name/${inputValue}?fields=name,capital,population,flags,languages`;
-
   return fetch(URL).then(manageErrors).then((response) => response.json());
 }
-
-export default fetchCountries;

3d87cc0eac639b99323bfc87674edabd1b2c5784
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-02-14 22:45:22 +0200

    update cards.js

--- src/cards.js
+++ src/cards.js
@@ -1,39 +1,45 @@
-export {countryCard, countryItem};
-
-function countryItem({ name, flags }) {
-  const nameCountry = name.official;
-  const flagCountry = flags.svg;
+export { countryCard, countryItem };
   
-  return `
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+function countryItem(cards) {
+  const cardUl = cards.map(({ name, flags }) => {
+    return `
     <li class='hero'>
       <img            
-        src="${flagCountry}"
+        src="${flags.svg}"
         alt="flag"
         width="40"
         height="20"
       />
-      <h1>${nameCountry}</h1>
+      <h1>${name.official}</h1>
     </li>
-    `
+    `})
+    .join('');
+  
+    countryList.innerHTML = cardUl;
+    countryInfo.innerHTML = '';
 }
 
-function countryCard({ name, capital, population, flags, languages }) {
-  const languagesCountry = Object.values(languages).join(', ');
-  const nameCountry = name.official;
-  const flagCountry = flags.svg;
-  
-  return `
+function countryCard(card) {
+  const cardInfo = card.map(({ name, capital, population, flags, languages }) => {
+    return `
     <div class='hero'>
       <img            
-        src="${flagCountry}"
+        src="${flags.svg}"
         alt="flag"
         width="40"
         height="20"
       />
-      <h1>${nameCountry}</h1>
+      <h1>${name.official}</h1>
     </div>
     <p><b>Capital: </b>${capital}</p>
     <p><b>Population: </b>${population}</p>
-    <p><b>Languages: </b>${languagesCountry}</p>
-    `
+    <p><b>Languages: </b>${Object.values(languages).join(', ')}</p>
+    `})
+    .join('');
+  
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = cardInfo;
 }
